75dfd0c82f0dc176d8fab389d79f5af4a008e2d3
chithraks <chithra.sahadev@gmail.com>
2025-03-30 18:15:29 +1100

    End of section 2

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+[ApiController]
+[Route("api/[controller]")]
+public class BaseAPIController : ControllerBase
+{
+   
+
+
+    public BaseAPIController()
+    {
+    }  
+}
+}
